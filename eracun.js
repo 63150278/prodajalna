66c9ecde0e826eb9a649e6639f5ae0dc86b8fcf9
Saula <ms1004@student.uni-lj.si>
2016-05-08 09:16:43 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -195,6 +195,8 @@ var vrniRacune = function(callback) {
   );
 }
 
+var sporociloPoRegistraciji;
+
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
@@ -211,11 +213,29 @@ streznik.post('/prijava', function(zahteva, odgovor) {
       //TODO: add fields and finalize
       //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
       //stmt.finalize();
+      
+      stmt.run(polja["FirstName"], polja["LastName"], polja["Company"], polja["Address"], polja["City"], 
+               polja["State"], polja["Country"], polja["PostalCode"], polja["Phone"], 
+               polja["Fax"], polja["Email"], 3);
+               
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
+    
+    if (napaka2 == false) {
+      sporociloPoRegistraciji = "Stranka je bila uspešno registrirana."; 
+    }
+    else {
+      sporociloPoRegistraciji = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+    }
+    
+    vrniStranke(function(napaka1, stranke) {
+      vrniRacune(function(napaka2, racuni) {
+        odgovor.render('prijava', {sporocilo: sporociloPoRegistraciji, seznamStrank: stranke, seznamRacunov: racuni});  
+      }) 
+    });
+
   });
 })
 
